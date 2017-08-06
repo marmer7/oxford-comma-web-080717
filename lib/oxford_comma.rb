8a3dabaf22f6b3de8500caef7bf6c725fee7ab78
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-06 17:55:56 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  l = array.length
+  if l == 1
+    return array.join
+  elsif l == 2
+    return array.join(" and ")
+  elsif l >= 3
+    t = ""
+    while array.length > 1
+      t += "#{array.shift}, "
+    end
+    t += "and #{array.shift}"
+    return t
+  else
+  end
 end
